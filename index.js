0b0e4aac21fb78e1abe53ad4ccf09cffb9f8966d
Ta Minh Tri <tmtri92.td@gmail.com>
2021-05-17 16:14:48 +0700

    Update

---
+++ index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { View, StyleSheet } from "react-native";
+
+export default class Test extends Component {
+  render() {
+    return <View>Test</View>;
+  }
+}
